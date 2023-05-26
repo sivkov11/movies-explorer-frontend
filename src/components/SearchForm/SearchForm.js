806deb9490e7b123b167dc9f6670c5134c1fbd9a
Sivkov Ilya <sivkov11@yandex.ru>
2023-05-26 12:33:19 +0700

    правки после 2 ревью

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,7 +5,7 @@ function SearchForm() {
   return (
     <section className="search">
       <form className="search__container">
-        <input className="search__input" placeholder="Фильм" type="text"/>
+        <input className="search__input" placeholder="Фильм" type="text" required/>
         <button type="submit" className="search__button"></button>
       </form>
       <FilterCheckbox/>
